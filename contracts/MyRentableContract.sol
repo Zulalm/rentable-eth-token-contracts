4daa745cd8dfd201038331903efa2db7c369399c
Zulalm <zulalmolla@gmail.com>
2024-03-24 16:45:45 +0300

    double link list

--- contracts/MyRentableContract.sol
+++ contracts/MyRentableContract.sol
@@ -7,8 +7,11 @@ contract MyRentableToken is ERC20 {
     struct Node{
         RentedToken token;
         uint256 next;
+        uint256 prev;
     }
 
+    uint256 MAX_NO_NODES = 1000000;
+    uint256 MAX_NO_RENTINGS = 1000;
 
     mapping(address account => Balance) private _balances;
     mapping(uint256 => Node) private nodes;
@@ -19,10 +22,12 @@ contract MyRentableToken is ERC20 {
         uint256 tokens;
         uint256 rentedHead;
         uint256 borrowedHead;
+        uint256 noRentedTokens;
     }
 
     struct RentedToken {
-        address renterOrBorrower;
+        address account;
+        uint256 renterOrBorrower;
         uint256 startDate;
         uint256 endDate;
         uint256 amount;
@@ -84,6 +89,9 @@ contract MyRentableToken is ERC20 {
         if (balanceOfInterval(owner, startDate, endDate) < value)  {
             return false;
         }
+        else if(_balances[owner].noRentedTokens == MAX_NO_RENTINGS || _balances[to].noRentedTokens == MAX_NO_RENTINGS){
+            return false;
+        }
         else{
             uint256 nodeId1;
             uint256 nodeId2;
@@ -106,8 +114,15 @@ contract MyRentableToken is ERC20 {
             nodes[nodeId1].next = ownerRentedHead;
             nodes[nodeId2].next = toBorrowedHead;
 
-            nodes[nodeId1].token = RentedToken(to, startDate,endDate,value);
-            nodes[nodeId2].token = RentedToken(owner, startDate,endDate,value);
+            nodes[ownerRentedHead].prev = nodeId1;
+            nodes[toBorrowedHead].prev = nodeId2;
+
+            nodes[nodeId1].token = RentedToken(to, nodeId2, startDate,endDate,value);
+            nodes[nodeId2].token = RentedToken(owner, nodeId1, startDate,endDate,value);
+
+            _balances[owner].noRentedTokens++;
+            _balances[to].noRentedTokens++;
+
             return true;
         }
     }
@@ -115,26 +130,31 @@ contract MyRentableToken is ERC20 {
         address account = msg.sender;
         bool refunded = false;
         uint256 borrowedIterator = _balances[account].borrowedHead;
-        uint256 prevBorrowedIterator = uint256(0);
-
         while(borrowedIterator != uint256(0)){
             RentedToken memory token = nodes[borrowedIterator].token;
             if(token.startDate < block.timestamp && token.endDate < block.timestamp){
                 // token should be refunded
                 refunded = true;
+                uint256 prevBorrowedIterator = nodes[borrowedIterator].prev;
                 if(prevBorrowedIterator == uint256(0)){
                     // refunded token was the head of the borrowed list
                     uint256 next = nodes[borrowedIterator].next;
                     addNodeToFreeList(borrowedIterator);
                     borrowedIterator = next;
                     _balances[account].borrowedHead = borrowedIterator;
+                    nodes[borrowedIterator].prev = uint256(0);
                 }else{
                     nodes[prevBorrowedIterator].next = nodes[borrowedIterator].next;
+                    nodes[nodes[borrowedIterator].next].prev = prevBorrowedIterator;
                     addNodeToFreeList(borrowedIterator);
                     borrowedIterator = nodes[prevBorrowedIterator].next;
+
                 }
+                _balances[account].noRentedTokens--;
+
+                //renter should get refund the token to avoid conflicts
+                getRefund(token.renterOrBorrower);
             }else{
-                prevBorrowedIterator = borrowedIterator;
                 borrowedIterator = nodes[borrowedIterator].next;
             }
             
@@ -146,31 +166,87 @@ contract MyRentableToken is ERC20 {
         address account = msg.sender;
         bool refunded = false;
         uint256 rentedIterator = _balances[account].rentedHead;
-        uint256 prevRentedIterator = uint256(0);
-
         while(rentedIterator != uint256(0)){
             RentedToken memory token = nodes[rentedIterator].token;
             if(token.startDate < block.timestamp && token.endDate < block.timestamp){
                 // token should be refunded
                 refunded = true;
+                uint256 prevRentedIterator = nodes[rentedIterator].prev;
                 if(prevRentedIterator == uint256(0)){
                     // refunded token was the head of the borrowed list
                     uint256 next = nodes[rentedIterator].next;
                     addNodeToFreeList(rentedIterator);
                     rentedIterator = next;
                     _balances[account].rentedHead = rentedIterator;
+                    nodes[rentedIterator].prev = uint256(0);
                 }else{
                     nodes[prevRentedIterator].next = nodes[rentedIterator].next;
+                    nodes[nodes[rentedIterator].next].prev = prevRentedIterator;
                     addNodeToFreeList(rentedIterator);
                     rentedIterator = nodes[prevRentedIterator].next;
                 }
+                 _balances[account].noRentedTokens--;
+                //borrower should refund the token to avoid conflicts
+                refund(token.renterOrBorrower);
             }else{
-                prevRentedIterator = rentedIterator;
                 rentedIterator = nodes[rentedIterator].next;
             }
         }
         return refunded;
     }
+
+    function refund(uint256 nodeId)  virtual internal returns (bool){
+        bool refunded = false;
+        RentedToken memory token = nodes[nodeId].token;
+        address account = token.account;
+        if(token.startDate < block.timestamp && token.endDate < block.timestamp){
+                // token should be refunded
+                refunded = true;
+                uint256 prevBorrowedIterator = nodes[nodeId].prev;
+                if(prevBorrowedIterator == uint256(0)){
+                    // refunded token was the head of the borrowed list
+                    uint256 next = nodes[nodeId].next;
+                    addNodeToFreeList(nodeId);
+                    nodeId = next;
+                    _balances[account].borrowedHead = nodeId;
+                    nodes[nodeId].prev = uint256(0);
+                }else{
+                    nodes[prevBorrowedIterator].next = nodes[nodeId].next;
+                    addNodeToFreeList(nodeId);
+                    nodeId = nodes[prevBorrowedIterator].next;
+                }
+             _balances[account].noRentedTokens--;
+            }
+            
+        return refunded;
+    }
+
+    function getRefund(uint256 nodeId) virtual internal returns (bool){
+        
+        bool refunded = false;
+        RentedToken memory token = nodes[nodeId].token;
+        address account = token.account;
+             if(token.startDate < block.timestamp && token.endDate < block.timestamp){
+                // token should be refunded
+                refunded = true;
+                uint256 prevRentedIterator = nodes[nodeId].prev;
+                if(prevRentedIterator == uint256(0)){
+                    // refunded token was the head of the rented list
+                    uint256 next = nodes[nodeId].next;
+                    addNodeToFreeList(nodeId);
+                    nodeId = next;
+                    _balances[account].rentedHead = nodeId;
+                    nodes[nodeId].prev = uint256(0);
+                }else{
+                    nodes[prevRentedIterator].next = nodes[nodeId].next;
+                    addNodeToFreeList(nodeId);
+                    nodeId = nodes[prevRentedIterator].next;
+                }
+                _balances[account].noRentedTokens--;
+            }
+        return refunded;
+    }
+
  function _update(address from, address to, uint256 value) override internal virtual {
         if (from == address(0)) {
             // Overflow check required: The rest of the code assumes that totalSupply never overflows
@@ -204,12 +280,16 @@ contract MyRentableToken is ERC20 {
     function getNodeFromFreeList() internal returns(uint256 nodeId){
         uint256 next = nodes[freeListHead].next;
         nodes[freeListHead].next = uint256(0);
+        nodes[freeListHead].prev = uint256(0);
         nodeId = freeListHead;
         freeListHead = next;
+        nodes[freeListHead].prev = uint256(0);
         return nodeId;
     }
     function addNodeToFreeList(uint256 nodeId) internal{
         nodes[nodeId].next = freeListHead;
+        nodes[nodeId].prev = uint256(0);
+        nodes[freeListHead].prev = nodeId;
         freeListHead = nodeId;
     }
 
